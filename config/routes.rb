19dfcbdd9090c15944e691aed13d069d5d0821f9
unknown <sohailshahidi1435@gmail.com>
2023-06-12 23:15:32 +0430

    Update uotes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :entries
-  resources :groups
-  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
+  resources :users do
+    resources :groups, path: 'categories', only: %i[index show new create edit update destroy] do
+      resources :entries, path: 'transactions', only: %i[index show new create edit update destroy]
+    end
+  end
+
   root 'splash#index'
 end
