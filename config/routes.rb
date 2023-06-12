3dcd2bbcbefa99c2ec42ddb5a806eee19f72d9b3
unknown <sohailshahidi1435@gmail.com>
2023-06-12 23:33:21 +0430

    Update routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  devise_for :users
+  
   resources :users do
     resources :groups, path: 'categories', only: %i[index show new create edit update destroy] do
       resources :entries, path: 'transactions', only: %i[index show new create edit update destroy]
